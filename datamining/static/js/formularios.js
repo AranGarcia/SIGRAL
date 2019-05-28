9a08a6d73b4207fd7b380dbc69079a50603ce787
Aran Garcia <agarciaa1509@alumno.ipn.mx>
2019-05-28 16:04:35 -0500

    Archivos estáticos, cambio de dashboard

---
+++ datamining/static/js/formularios.js
@@ -0,0 +1,16 @@
+$("#ancla-2").on('click', function () {
+    $('#formularios').load(formulario2);
+});
+
+$("#ancla-3").on('click', function () {
+    $('#formularios').load(formulario3);
+});
+$("#ancla-4").on('click', function () {
+    $('#formularios').load(formulario4);
+});
+$("#ancla-5").on('click', function () {
+    $('#formularios').load(formulario5);
+});
+$("#ancla-6").on('click', function () {
+    $('#formularios').load(formulario6);
+});
