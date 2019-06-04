d64ef8bd2fbb0fcc10457c84aa1802de69ceda90
Aran Garcia <agarciaa1509@alumno.ipn.mx>
2019-06-04 00:56:27 -0500

    Validación primera consulta

--- datamining/static/js/peticiones/formulario1.js
+++ datamining/static/js/peticiones/formulario1.js
@@ -1,10 +1,24 @@
 $(function () {
+    $('.alert').alert();
+    // AJAX para primera consulta
     var form = $('#mdxquery1');
-
     $(form).submit(function (event) {
         event.preventDefault();
 
         var formData = $(form).serialize();
+
+        var lanio = parseInt(document.getElementById('lanio').value);
+        var ranio = parseInt(document.getElementById('ranio').value);
+        if (lanio > ranio) {
+            $('#alert_placeholder').html(
+                '<div class="alert alert-danger alert-dismissible" role="alert">\
+                    Rango de a&ntilde;os inv&aacute;lidos.\
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">\
+                        <span aria-hidden="true">&times;</span>\
+                    </button>\
+                </div>')
+            return;
+        }
 
         $.ajax({
             type: 'GET',
